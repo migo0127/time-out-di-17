37c11f05d18d59aeb820de030ac422597f215f67
migo0127 <migo0127@gamil.com>
2024-06-01 16:57:48 +0800

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-V32S3JGF.mjs";import"./chunk-SF2KHYVJ.mjs";import"./chunk-KX3WRALE.mjs";import"./chunk-2LMWK4L3.mjs";import"./chunk-TC5CWH7P.mjs";import"./chunk-K6RM6F3Q.mjs";import"./chunk-ZPTDKJIY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
